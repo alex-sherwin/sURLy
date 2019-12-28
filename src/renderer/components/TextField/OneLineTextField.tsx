bc346ebd5b33a25c7d8e2016c56daecaf64a88b8
Alex Sherwin <alex-sherwin@users.noreply.github.com>
2019-12-28 16:38:43 -0500

    better variable tracking... almost there...

--- src/renderer/components/TextField/OneLineTextField.tsx
+++ src/renderer/components/TextField/OneLineTextField.tsx
@@ -1,7 +1,7 @@
 // third party
-import React, { FC, useState, useRef } from "react";
+import React, { FC, useRef } from "react";
 import MonacoEditor, { MonacoEditorProps } from 'react-monaco-editor';
-import monacoEditor from "monaco-editor";
+import monacoEditor, { Range, Selection } from "monaco-editor";
 import _sortBy from "lodash/sortBy";
 
 // local
@@ -30,55 +30,60 @@ const EMPTY_VARS: Var[] = [];
 export const OneLineTextField: FC<OneLineTextFieldProps> = (props) => {
 
   const editorValue = useRef<string>("http://{hostname}.com:{port}/http/some-thing/_herewego?value=abc%20123");
-  const [editor, setEditor] = useState<monacoEditor.editor.IStandaloneCodeEditor | null>(null);
+  const editorRef = useRef<monacoEditor.editor.IStandaloneCodeEditor | null>(null);
   const monacoRef = useRef<typeof monacoEditor | null>(null);
   const lastKeyCode = useRef<string | null>(null);
   const varTracking = useRef<VarTracking>({});
+  const varEditsPending = useRef<boolean>(false);
 
   const vars = useRef<Var[]>([
     { lineNumber: 1, start: 7, end: 17, display: "{hostname}", selected: false },
     { lineNumber: 1, start: 22, end: 28, display: "{port}", selected: false },
   ]);
 
+
+  const applyEditorDecorations = () => {
+
+    if (!editorRef.current) {
+      return;
+    }
+
+    const nextDecorations: monacoEditor.editor.IModelDeltaDecoration[] = vars.current.map((it) => ({
+      range: { startLineNumber: it.lineNumber, endLineNumber: it.lineNumber, startColumn: it.start + 1, endColumn: it.end + 1 },
+      options: {
+        className: it.selected ? "myDecoration selected" : "myDecoration",
+        inlineClassName: it.selected ? "myInlineDecoration selected" : "myInlineDecoration",
+        inlineClassNameAffectsLetterSpacing: true,
+        isWholeLine: false,
+        stickiness: monacoRef.current!.editor.TrackedRangeStickiness.NeverGrowsWhenTypingAtEdges,
+        hoverMessage: { value: "# wut", isTrusted: true },
+      },
+    } as monacoEditor.editor.IModelDeltaDecoration));
+
+    const result = editorRef.current.deltaDecorations(Object.keys(varTracking.current), nextDecorations);
+    // const result = editorRef.current.deltaDecorations([], nextDecorations);
+    const tracking: VarTracking = {};
+
+    for (let i = 0; i < result.length; i++) {
+      const id = result[i];
+      tracking[id] = vars.current[i];
+    }
+
+    varTracking.current = tracking;
+  };
+
   const onDidMount = (editor: monacoEditor.editor.IStandaloneCodeEditor, monaco: typeof monacoEditor) => {
 
     monacoRef.current = monaco;
-
-    setEditor(editor);
+    editorRef.current = editor;
 
     monaco.editor.defineTheme("custom", Custom);
     monaco.editor.setTheme("custom");
 
-    const applyEditorDecorations = () => {
-
-      const nextDecorations: monacoEditor.editor.IModelDeltaDecoration[] = vars.current.map((it) => ({
-        range: { startLineNumber: it.lineNumber, endLineNumber: it.lineNumber, startColumn: it.start + 1, endColumn: it.end + 1 },
-        options: {
-          className: it.selected ? "myDecoration selected" : "myDecoration",
-          inlineClassName: it.selected ? "myInlineDecoration selected" : "myInlineDecoration",
-          inlineClassNameAffectsLetterSpacing: true,
-          isWholeLine: false,
-          stickiness: monaco.editor.TrackedRangeStickiness.NeverGrowsWhenTypingAtEdges,
-          hoverMessage: { value: "# wut", isTrusted: true },
-        },
-      } as monacoEditor.editor.IModelDeltaDecoration));
-
-      const result = editor.deltaDecorations(Object.keys(varTracking.current), nextDecorations);
-      const tracking: VarTracking = {};
-
-      for (let i = 0; i < result.length; i++) {
-        const id = result[i];
-        tracking[id] = vars.current[i];
-      }
-
-      console.log("setting varTracking to", JSON.stringify(tracking));
-      varTracking.current = tracking;
-    };
-
     setImmediate(applyEditorDecorations);
 
     editor.onDidChangeModelContent((e) => {
-      console.log("onDidChangeModelContent", e);
+      // console.log("onDidChangeModelContent", e);
     });
 
     editor.onKeyDown((e) => {
@@ -101,13 +106,13 @@ export const OneLineTextField: FC<OneLineTextFieldProps> = (props) => {
             editor.setPosition({
               lineNumber,
               column: v.start + 1, // modify to 1-based index
-            })
+            });
           } else if (lastKeyCode.current === "ArrowRight") {
             // move to right side
             editor.setPosition({
               lineNumber,
               column: v.end + 1, // modify to 1-based index
-            })
+            });
           } else {
             // figure out left or right based on how close it is to the center
 
@@ -129,12 +134,12 @@ export const OneLineTextField: FC<OneLineTextFieldProps> = (props) => {
     });
 
     editor.onDidChangeCursorSelection((e) => {
-      console.log(`onDidChangeCursorSelection(reason=${e.reason}, source=${e.source})`)
+      // console.log(`onDidChangeCursorSelection(reason=${e.reason}, source=${e.source})`)
       if (hasSelection(e.selection)) {
         const selectedValue = editorValue.current.substring(Math.min(e.selection.startColumn, e.selection.endColumn) - 1, Math.max(e.selection.startColumn, e.selection.endColumn) - 1);
-        console.log(`** selection [${selectedValue}]`);
+        // console.log(`** selection [${selectedValue}]`);
       } else {
-        console.log(`-- no selection`);
+        // console.log(`-- no selection`);
       }
     });
 
@@ -177,54 +182,166 @@ export const OneLineTextField: FC<OneLineTextFieldProps> = (props) => {
 
     // disable find
     editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_F, function () { });
+    editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_G, function () { });
+    editor.addCommand(monaco.KeyMod.Shift | monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_G, function () { });
+
+    // disable goto line
+    editor.addCommand(monaco.KeyMod.WinCtrl | monaco.KeyCode.KEY_G, function () { });
 
     // disable select line
     editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_L, function () { });
+    editor.addCommand(monaco.KeyMod.Shift | monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_L, function () { });
 
-    // disable enter
-    editor.addCommand(monaco.KeyCode.Enter, function () { });
+    // disable enter (only during text editor focus)
+    editor.addCommand(monaco.KeyCode.Enter, function (e: any) { }, 'editorTextFocus && !suggestWidgetVisible');
     editor.addCommand(monaco.KeyMod.Shift | monaco.KeyCode.Enter, function () { });
 
     // disable command pallette
     editor.addCommand(monaco.KeyCode.F1, function () { });
 
+
+
     editor.onDidChangeModelDecorations(e => {
-      console.log("onDidChangeModelDecorations");
 
-      const currentDecorations = editor.getLineDecorations(1);
-
-      if (currentDecorations) {
-        for (const currentDecoration of currentDecorations) {
-          console.log("varTracking.current", JSON.stringify(varTracking.current));
-          const v = varTracking.current[currentDecoration.id];
-          if (!v) {
-            continue;
-          }
-          if (v.lineNumber !== currentDecoration.range.startLineNumber) {
-            console.log("updated line number");
-            v.lineNumber = currentDecoration.range.startLineNumber;
-          }
-          if (v.start !== currentDecoration.range.startColumn - 1) {
-            console.log("updated start col");
-            v.start = currentDecoration.range.startColumn - 1; // convert to 0-based indexing
-          }
-          if (v.end !== currentDecoration.range.endColumn - 1) {
-            console.log("updated end col");
-            v.end = currentDecoration.range.endColumn - 1; // convert to 0-based indexing
-          }
-        }
+      if (varEditsPending.current) {
+        return;
       }
+      try {
+        varEditsPending.current = true;
 
-      // // track changes to vars via model decoration changes
-      // const currentDecorations = editor!.getLineDecorations(1);
+        console.log(">> start onDidChangeModelDecorations");
 
+
+
+        const currentDecorations = editor.getLineDecorations(1);
+
+        if (currentDecorations) {
+
+          const toDeletes: { v: Var, decoration: monacoEditor.editor.IModelDecoration }[] = [];
+          let hasModifications = false;
+
+          for (const currentDecoration of currentDecorations) {
+            // console.log("varTracking.current", JSON.stringify(varTracking.current));
+            const v = varTracking.current[currentDecoration.id];
+            if (!v) {
+              continue;
+            }
+            const oldSize = v.end - v.start;
+            const newSize = currentDecoration.range.endColumn - currentDecoration.range.startColumn;
+
+            if (oldSize != newSize) {
+              // size changed, user wants to delete the var
+              console.log(`deleting id=${currentDecoration.id}`);
+              toDeletes.push({ v, decoration: currentDecoration });
+            } else {
+              // check if moved, track changes
+              if (v.lineNumber !== currentDecoration.range.startLineNumber) {
+                // console.log("updated line number");
+                v.lineNumber = currentDecoration.range.startLineNumber;
+              }
+              if (v.start !== currentDecoration.range.startColumn - 1) {
+                // console.log("updated start col");
+                v.start = currentDecoration.range.startColumn - 1; // convert to 0-based indexing
+              }
+              if (v.end !== currentDecoration.range.endColumn - 1) {
+                // console.log("updated end col");
+                v.end = currentDecoration.range.endColumn - 1; // convert to 0-based indexing
+              }
+            }
+          }
+
+
+          // let inFluxEditorValue = editor.getValue();
+
+          const edits: monacoEditor.editor.IIdentifiedSingleEditOperation[] = [];
+
+          if (toDeletes.length > 0) {
+
+            hasModifications = true;
+
+            const varsCopy = [...vars.current];
+
+            // delete from right-to-left so that editor text indexes can be processed without affecting one another
+            const sortedToDeletes = _sortBy(toDeletes, it => it.decoration.range.startColumn).reverse();
+
+            for (const toDelete of sortedToDeletes) {
+              const { v, decoration } = toDelete;
+
+              // splice out the var
+              const idx = varsCopy.indexOf(v);
+              if (idx !== -1) {
+                varsCopy.splice(idx, 1);
+              }
+
+              // modify the text model to remove the remainder of the var
+
+              // text before decoration
+              // const valueBefore = inFluxEditorValue.substring(0, decoration.range.startColumn - 1); // 0-based indexing
+
+              // // text after decoration
+              // const valueAfter = inFluxEditorValue.substring(decoration.range.endColumn - 1); // 0-based indexing
+
+              edits.push({
+                text: null,
+                range: new Range(decoration.range.startLineNumber, decoration.range.startColumn, decoration.range.endLineNumber, decoration.range.endColumn),
+              });
+
+              // setImmediate(() => {
+              //   editor.executeEdits(
+              //     "my-source",
+              //     [{
+              //       text: null,
+              //       range: new Range(decoration.range.startLineNumber, decoration.range.startColumn, decoration.range.endLineNumber, decoration.range.endColumn),
+              //     }],
+              //     (ops) => {
+              //       console.log("ops", ops);
+              //       return [new Selection(1, 1, 1, 2)];
+              //     }
+              //   );
+              // });
+
+
+              // inFluxEditorValue = valueBefore + valueAfter;
+
+            }
+
+            vars.current = varsCopy;
+          }
+
+          if (hasModifications) {
+            // editor.setValue(inFluxEditorValue);
+            setImmediate(() => {
+              editor.executeEdits(
+                "my-source", edits,
+                (ops) => {
+                  // const nextSelections:Selection[] = 
+                  return ops.map(it => new Selection(it.range.startLineNumber, it.range.startColumn, it.range.endLineNumber, it.range.endColumn));
+                }
+              );
+              applyEditorDecorations();
+              varEditsPending.current = false;
+            });
+
+          } else {
+            varEditsPending.current = false;
+
+          }
+
+        }
+
+        // // track changes to vars via model decoration changes
+        // const currentDecorations = editor!.getLineDecorations(1);
+
+        console.log("<< end onDidChangeModelDecorations");
+      } finally {
+      }
 
     });
 
   };
 
   const onChange = (value: string, event: monacoEditor.editor.IModelContentChangedEvent) => {
-    console.log("onChange");
+    // console.log("onChange");
 
     // const decorations = editor!.getLineDecorations(1);
     // console.log("decorations", decorations);
